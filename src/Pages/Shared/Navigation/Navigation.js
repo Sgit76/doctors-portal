1986b99df92337b785fbbd886af9c0aed4673347
Sgit76 <shafiul.cse76@gmail.com>
2022-04-02 09:05:05 +0600

    second commit

--- src/Pages/Shared/Navigation/Navigation.js
+++ src/Pages/Shared/Navigation/Navigation.js
@@ -7,28 +7,38 @@ import Button from '@mui/material/Button';
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
 import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
+import useAuth from '../../../hooks/useAuth';
 const Navigation = () => {
+  const {user , logOut} = useAuth();
     return (
-        <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static">
-        <Toolbar>
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            sx={{ mr: 2 }}
-          >
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            Doctors Portal
-          </Typography>
-          <Link to="appointment"><Button color="inherit">Appointment</Button></Link>
-          <Button color="inherit">Login</Button>
-        </Toolbar>
-      </AppBar>
-    </Box>
+      <Box sx={{ flexGrow: 1 }}>
+        <AppBar position="static">
+          <Toolbar>
+            <IconButton
+              size="large"
+              edge="start"
+              color="inherit"
+              aria-label="menu"
+              sx={{ mr: 2 }}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+              Doctors Portal
+            </Typography>
+            <Link style={{textDecoration:'none' , color: 'white'}} to="/appointment"><Button color="inherit">Appointment</Button></Link>
+          {
+            user?.email ? 
+              <Button onClick={logOut} color="inherit">Logout</Button>
+            :
+            <NavLink style={{textDecoration:'none' , color: 'white'}} to="/login">
+              <Button color="inherit">Login</Button>
+            </NavLink> 
+          }
+          </Toolbar>
+        </AppBar>
+      </Box>
     );
 };
 
---
+++ src/hooks/useFirebase.js
@@ -0,0 +1,74 @@
+import { useEffect, useState } from "react";
+import initializeFirebase from "../Pages/Login/Firebase/firebase.init";
+import { getAuth, onAuthStateChanged  ,createUserWithEmailAndPassword , signInWithEmailAndPassword ,signOut} from "firebase/auth";
+initializeFirebase();
+
+
+const useFirebase = () =>{
+    const [user, setUser] = useState({});
+    const [isLoading, setIsLoading] = useState(true);
+    const [authError , setAuthError] = useState('');
+    const auth = getAuth();
+
+    const registerUser = (email, password) =>{
+        setIsLoading(true);
+        createUserWithEmailAndPassword(auth , email, password)
+        .then((userCredential) => {
+           setAuthError('');
+          })
+          .catch((error) => {
+            setAuthError(error.message);
+            // ..
+          })
+          .finally(() => setIsLoading(false));
+    }
+
+    const loginUser = (email, password) => {
+        setIsLoading(true);
+        signInWithEmailAndPassword(auth, email, password)
+        .then((userCredential) => {
+          setAuthError('');
+        })
+        .catch((error) => {
+            setAuthError(error.message);
+        })
+        .finally(() => setIsLoading(false));
+    }
+
+
+
+    //observer user state
+    useEffect(() => {
+    const unsubscribed = onAuthStateChanged(auth, (user) => {
+            if (user) {
+              setUser(user);
+            } else {
+             setUser({})
+            }
+            setIsLoading(false);
+          });
+          return () => unsubscribed;
+    } , [])
+    
+    const logOut = ()=>{
+        setIsLoading(true);
+        signOut(auth).then(() => {
+            
+            alert('Sign-out successful');
+          }).catch((error) => {
+            
+          })
+          .finally(() => setIsLoading(false));
+    }
+
+    return {
+        user,
+        isLoading,
+        authError,
+        registerUser,
+        loginUser,
+        logOut,
+    }
+}
+
+export default useFirebase;
